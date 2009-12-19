14e2221f53546e9b4184f1b43e019aed98a98e2a
Holger Just <redmine@meine-er.de>
2009-12-19 01:49:34 +0000

    Moved javascript function directly into onchange event as the free function is not generated correctly in ajax includes.
    git-svn-id: http://dev.holgerjust.de/svn/redmine-checkout/trunk@16 2c10314b-d60d-4f8f-b362-a6b3e82123ed

--- lib/checkout_repositories_helper_patch.rb
+++ lib/checkout_repositories_helper_patch.rb
@@ -12,18 +12,9 @@ module RepositoriesHelperPatch
   module InstanceMethods
     def repository_field_tags_with_checkout(form, repository)    
       tags = repository_field_tags_without_checkout(form, repository) || ""
-      return tags unless tags.blank?
+      return tags if repository.class.name == "Repository"
       
-      js = <<-EOF
-      <script type="text/javascript">
-        function show_checkout_url() {
-          var txt = $('repository_checkout_url');
-          if (txt.value.empty()) {txt.value = $('repository_url').value;}
-          $('checkout_url').show();
-        }
-      </script>
-      EOF
-      tags + js +
+      tags +
       content_tag('p',
         form.select(:checkout_url_type, [
             [l(:label_checkout_type_original), 'original'],
@@ -31,7 +22,15 @@ module RepositoriesHelperPatch
             [l(:label_checkout_type_overwritten), 'overwritten']
           ],
           {},
-          :onchange => "if ($A(['original', 'none']).include(value)){$('checkout_url').hide();} else {show_checkout_url();}"
+          :onchange => <<-EOF
+            if ($A(['original', 'none']).include(value)){
+              $('checkout_url').hide();
+            } else {
+              var txt = $('repository_checkout_url');
+              if (txt.value.empty()) {txt.value = $('repository_url').value;}
+              $('checkout_url').show();
+            }
+            EOF
         )
       ) +
       content_tag('p',
