76e4f0d724f3d584cd70dd9124646e39b24b84aa
Holger Just <redmine@meine-er.de>
2010-01-19 01:22:59 +0000

    Fixed failing migration.
    git-svn-id: http://dev.holgerjust.de/svn/redmine-checkout/trunk@27 2c10314b-d60d-4f8f-b362-a6b3e82123ed

--- db/migrate/20100118235909_add_overwrite_option.rb
+++ db/migrate/20100118235909_add_overwrite_option.rb
@@ -1,12 +1,10 @@
 class AddOverwriteOption < ActiveRecord::Migration
-  class Repository < ActiveRecord::Base
-  end
-
   def self.up
     add_column :repositories, :checkout_url_overwrite, :boolean, :default => false, :null => false
     
     # existing repositories are set to overwrite the default settings
     # This is to keep continuity of settings.
+    Repository.reset_column_information
     Repository.all.each{|r| r.update_attribute(:checkout_url_overwrite, true)}
   end
   
