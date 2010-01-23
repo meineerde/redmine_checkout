14108bceac49a55d028903bf30eac0bee8ef4a26
Holger Just <redmine@meine-er.de>
2010-01-23 14:47:41 +0000

    Stripped the large after_initialize method, created smaller lazy-evaluation methods.
    git-svn-id: http://dev.holgerjust.de/svn/redmine-checkout/trunk@32 2c10314b-d60d-4f8f-b362-a6b3e82123ed

--- lib/checkout_repository_patch.rb
+++ lib/checkout_repository_patch.rb
@@ -3,22 +3,48 @@ require_dependency 'repository'
 module RepositoryPatch
   def self.included(base) # :nodoc:
     base.send(:include, InstanceMethods)
+    
+    base.class_eval do
+      unloadable
+      
+      validates_inclusion_of :checkout_url_type, :in => %w(none original overwritten), :allow_nil => true
+      validates_inclusion_of :display_login, :in => %w(none username password), :allow_nil => true
+    end
   end
   
   module InstanceMethods
     def after_initialize
       self.checkout_url_overwrite ||= false
-      
-      unless self.checkout_url_overwrite
-        self.checkout_url_type = (Setting.plugin_redmine_checkout['checkout_url_type'])
-        self.display_login = (Setting.plugin_redmine_checkout['display_login'])
-        self.render_link = (Setting.plugin_redmine_checkout['render_link'] == "true")
-        if self.checkout_url_type == "overwritten"
-          self.checkout_url = generated_checkout_url
-        end
+    end
+    
+    def checkout_url_type
+      self.checkout_url_overwrite && read_attribute("checkout_url_type") || begin
+        Setting.plugin_redmine_checkout['checkout_url_type']
+      end
+    end
+
+    def display_login
+      self.checkout_url_overwrite && read_attribute("display_login") || begin
+        Setting.plugin_redmine_checkout['display_login']
       end
     end
     
+    def render_link
+      self.checkout_url_overwrite && read_attribute("render_link") || begin
+        Setting.plugin_redmine_checkout['render_link'] == "true"
+      end
+    end
+    
+    def checkout_url
+      self.checkout_url_overwrite && read_attribute("checkout_url") || begin
+        if self.checkout_url_type == "overwritten"
+          generated_checkout_url
+        else
+          self.root_url
+        end
+      end
+    end
+
     def generated_checkout_url
       regex = Setting.plugin_redmine_checkout['checkout_url_regex']
       replacement = Setting.plugin_redmine_checkout['checkout_url_regex_replacement']
