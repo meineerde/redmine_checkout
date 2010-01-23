c6c2e57bda77d2c116730701e06b58f5d28297b2
Holger Just <redmine@meine-er.de>
2010-01-23 14:48:02 +0000

    Added RSpec tests
    git-svn-id: http://dev.holgerjust.de/svn/redmine-checkout/trunk@33 2c10314b-d60d-4f8f-b362-a6b3e82123ed

---
+++ spec/models/repository_spec.rb
@@ -0,0 +1,39 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Repository do
+  fixtures :settings, :repositories
+  
+  describe "initialize" do
+    before(:each) do
+      @repo = Repository.new()
+    end
+
+    it "should properly set default values" do
+      @repo.checkout_url_type.should eql "overwritten"
+      @repo.display_login.should eql "username"
+      @repo.render_link.should be_false
+      @repo.checkout_url.should eql ""
+      @repo.checkout_url_overwrite.should be_false
+    end
+  end
+  
+  describe "checkout_url" do
+    before(:each) do
+      @repo = repositories :svn
+    end
+    
+    it "should be generated from root_url" do
+      @repo.root_url = "svn://example.com/svn/testrepo"
+      @repo.checkout_url_overwrite = false
+      
+      @repo.checkout_url.should eql "http://example.com/svn/testrepo"
+    end
+    
+    it "should respect overwritten setting" do
+      @repo.checkout_url = "http://example.com/svn/testrepo"
+      @repo.checkout_url_overwrite = true
+
+      @repo.checkout_url.should eql "http://example.com/svn/testrepo"
+    end
+  end
+end
