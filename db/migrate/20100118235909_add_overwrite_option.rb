3d428dbcff4681cd16fff784e591612cae2ba42e
Holger Just <redmine@meine-er.de>
2010-05-24 22:40:12 +0000

    Fixed migration for new installs (#145)
    git-svn-id: http://dev.holgerjust.de/svn/redmine-checkout/trunk@71 2c10314b-d60d-4f8f-b362-a6b3e82123ed

--- db/migrate/20100118235909_add_overwrite_option.rb
+++ db/migrate/20100118235909_add_overwrite_option.rb
@@ -1,4 +1,16 @@
 class AddOverwriteOption < ActiveRecord::Migration
+  class Repository < ActiveRecord::Base
+    def self.inheritance_column
+      # disable single table inheritance
+      nil
+    end
+    
+    # Need to mock because later code removes the attribute
+    def checkout_settings
+      {}
+    end
+  end
+  
   def self.up
     add_column :repositories, :checkout_url_overwrite, :boolean, :default => false, :null => false
     
