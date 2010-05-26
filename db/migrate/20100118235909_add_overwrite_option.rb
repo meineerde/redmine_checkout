cf9342f6cfc7bf5b12fca9135805271a488c2c85
Holger Just <redmine@meine-er.de>
2010-05-26 10:38:03 +0000

    Better migration for #145. Do not instantiate the repositories, thus not mock the Repository class.
    git-svn-id: http://dev.holgerjust.de/svn/redmine-checkout/trunk@73 2c10314b-d60d-4f8f-b362-a6b3e82123ed

--- db/migrate/20100118235909_add_overwrite_option.rb
+++ db/migrate/20100118235909_add_overwrite_option.rb
@@ -1,23 +1,11 @@
 class AddOverwriteOption < ActiveRecord::Migration
-  class Repository < ActiveRecord::Base
-    def self.inheritance_column
-      # disable single table inheritance
-      nil
-    end
-    
-    # Need to mock because later code removes the attribute
-    def checkout_settings
-      {}
-    end
-  end
-  
   def self.up
     add_column :repositories, :checkout_url_overwrite, :boolean, :default => false, :null => false
     
     # existing repositories are set to overwrite the default settings
     # This is to keep continuity of settings.
     Repository.reset_column_information
-    Repository.all.each{|r| r.update_attribute(:checkout_url_overwrite, true)}
+    Repository.update_all({:checkout_url_overwrite, true})
   end
   
   def self.down
