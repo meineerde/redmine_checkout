5b77981d4d40aa684cd7034e7109e90385634467
Holger Just <redmine@meine-er.de>
2010-06-28 14:50:22 +0000

    Made access tests dependent on a provided user instead of User.current. Protocol.access_rw returns nil if no access is allowed.
    git-svn-id: http://dev.holgerjust.de/svn/redmine-checkout/branches/feature-github@96 2c10314b-d60d-4f8f-b362-a6b3e82123ed

--- lib/checkout/repository_hooks.rb
+++ lib/checkout/repository_hooks.rb
@@ -7,23 +7,22 @@ module Checkout
     # * :repository => Current Repository
     #
     def view_repositories_show_contextual(context={})
-      unless context[:repository].blank? || !Setting.checkout_display_checkout_info
+      if context[:repository].present? && Setting.checkout_display_checkout_info
         protocols = context[:repository].checkout_protocols.select do |p|
-          p.access_rw == 'read-only' ||
-          p.access_rw == 'read+write' && User.current.allowed_to?(:commit_access, context[:repository].project)
+          p.access_rw(User.current)
         end
-      
+        
         path = context[:controller].instance_variable_get("@path")
         if path && context[:controller].instance_variable_get("@entry")
           # a single file is showing, so we return only the directory
           path = File.dirname(path)
         end
-      
-        default_protocol = protocols.find(&:default?) || protocols.first
-      
+        
+        default = protocols.find(&:default?) || protocols.first
+        
         context.merge!({
-          :checkout_protocols => protocols,
-          :default_checkout_protocol => default_protocol,
+          :protocols => protocols,
+          :default_protocol => default,
           :checkout_path => path
         })
       
