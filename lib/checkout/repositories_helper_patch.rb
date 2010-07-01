a0261a814c133c143c73b13b2b4c4f37281d604b
Holger Just <redmine@meine-er.de>
2010-07-01 11:53:13 +0000

    Now load subform.js correctly, even if no repository was defined yet in project settings.
    git-svn-id: http://dev.holgerjust.de/svn/redmine-checkout/branches/feature-github@107 2c10314b-d60d-4f8f-b362-a6b3e82123ed

--- lib/checkout/repositories_helper_patch.rb
+++ lib/checkout/repositories_helper_patch.rb
@@ -7,6 +7,7 @@ module Checkout
 
       base.class_eval do
         alias_method_chain :repository_field_tags, :checkout
+        alias_method_chain :scm_select_tag, :javascript
       end
     end
   
@@ -16,6 +17,13 @@ module Checkout
         return tags if repository.class.name == "Repository"
       
         tags + @controller.send(:render_to_string, :partial => 'projects/settings/repository_checkout', :locals => {:form => form, :repository => repository, :scm => repository.scm_name})
+      end
+      
+      def scm_select_tag_with_javascript(*args)
+        content_for :header_tags do
+          javascript_include_tag 'subform', :plugin => 'redmine_checkout'
+        end
+        scm_select_tag_without_javascript(*args)
       end
     end
   end
