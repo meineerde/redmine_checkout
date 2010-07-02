edf782fb73d545e614712c814b7ddef94c1a98a1
Holger Just <redmine@meine-er.de>
2010-07-02 00:06:17 +0000

    Added copy-to-clipboard feature using ZeroClipboard by Joseph Huckaby (http://code.google.com/p/zeroclipboard/) (#144)
    git-svn-id: http://dev.holgerjust.de/svn/redmine-checkout/branches/feature-github@110 2c10314b-d60d-4f8f-b362-a6b3e82123ed

--- assets/javascripts/checkout.js
+++ assets/javascripts/checkout.js
@@ -19,4 +19,14 @@ document.observe("dom:loaded", function() {
    this.focus();
    this.select();
   });
+
+  $('clipboard_container').show();
+  clipboard = new ZeroClipboard.Client();
+  clipboard.setHandCursor( true );
+  clipboard.glue('clipboard_button', 'clipboard_container');
+  
+  clipboard.addEventListener('mouseOver', function (client) {
+    clipboard.setText( $('checkout_url').value );
+  });
 });
+
