eca721cd5d92b7d80b86ca61e833ddbf02031d7a
Holger Just <hjust@meine-er.de>
2012-07-31 10:18:13 +0200

    Fix trailing whitespace

--- lib/checkout/protocol.rb
+++ lib/checkout/protocol.rb
@@ -5,7 +5,7 @@ module Checkout
       true
     end
   end
-  
+
   class Protocol
     attr_accessor :protocol, :regex, :regex_replacement, :access, :display_login, :repository
     attr_writer :default, :command, :fixed_url, :append_path
@@ -17,31 +17,31 @@ module Checkout
       END_SRC
       class_eval src, __FILE__, __LINE__
     end
-    
-    
+
+
     def initialize(args={})
       args = args.dup
-      
+
       @protocol = args.delete :protocol
       @command = args.delete :command # optional, if not set the default from the repo is used
-      
+
       # either a fixed url
       @fixed_url = args.delete :fixed_url
       # or a regex
       @regex = args.delete :regex
       @regex_replacement = args.delete :regex_replacement
-      
+
       @access = args.delete :access
-      
+
       # boolean values
       @default = args.delete :is_default
       @append_path = args.delete :append_path
       @display_login = args.delete :display_login
-      
+
       # some reference
       @repository = args.delete :repository
     end
-    
+
     def full_command(path = "")
       cmd = ""
       if repository.checkout_display_command?
@@ -49,11 +49,11 @@ module Checkout
       end
       cmd + URI.escape(self.url(path))
     end
-    
+
     def command
       @command || self.repository && self.repository.checkout_default_command || ""
     end
-    
+
     def access_rw(user)
       # reduces the three available access levels 'read+write', 'read-only' and 'permission'
       # to 'read+write' and 'read-only' and nil (not allowed)
@@ -74,14 +74,14 @@ module Checkout
         @access
       end
     end
-    
+
     def access_label(user)
       case access_rw(user)
         when 'read+write': :label_access_read_write
         when 'read-only': :label_access_read_only
       end
     end
-  
+
     def fixed_url
       @fixed_url.present? ? @fixed_url : begin
         if (regex.blank? || regex_replacement.blank?)
@@ -96,12 +96,12 @@ module Checkout
 
     def url(path = "")
       return "" unless repository
-      
+
       url = fixed_url.sub(/\/+$/, "")
       if repository.allow_subtree_checkout? && self.append_path? && path.present?
         url = "#{url}/#{path}"
       end
-      
+
       if display_login?
         begin
           uri = URI.parse url
