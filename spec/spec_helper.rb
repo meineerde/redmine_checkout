00d60e04a4829ee0ab4ebea8833f5e958d5b5597
Christian Rijke <crijke@gmail.com>
2013-07-19 16:01:20 +0200

    Remove spec opts and migrate spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,26 +1,3 @@
-ENV['RAILS_ENV'] ||= 'test'
+# -- load spec_helper from OpenProject core
+require "spec_helper"
 
-# prevent case where we are using rubygems and test-unit 2.x is installed
-begin
-  require 'rubygems'
-  gem "test-unit", "~> 1.2.3"
-rescue LoadError
-end
-
-begin
-  require 'spec/spec_helper'
-rescue LoadError => error
-  puts <<-EOS
-
-    You need to install rspec in your Redmine project.
-    Please execute the following code:
-
-      gem install rspec-rails
-      script/generate rspec
-
-  EOS
-  raise error
-end
-
-Fixtures.create_fixtures File.join(File.dirname(__FILE__), "fixtures"), ActiveRecord::Base.connection.tables
-require File.join(File.dirname(__FILE__), "..", "init.rb")
